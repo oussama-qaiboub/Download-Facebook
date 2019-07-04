29dca6e57e09d550180ead3340cde725bef00795
unknown <oqaiboub@valueit.ma>
2019-07-04 22:45:31 +0100

    Download Facebook

---
+++ app/src/main/java/com/gyan/fbd/Constant.java
@@ -0,0 +1,14 @@
+package com.gyan.fbd;
+
+/**
+ * Created by Tushar on 9/10/2017.
+ */
+
+class Constant {
+    public static int nav_clicked = 0;
+    public static Boolean isNavClicked = false;
+
+    public static Boolean isToggle = true;
+    public static int color = 0xff3b5998;
+    public static int theme = R.style.AppTheme;
+}
---
+++ app/src/main/java/com/gyan/fbd/Constants.java
@@ -0,0 +1,10 @@
+package com.gyan.fbd;
+
+/**
+ * Created by Tushar on 9/7/2017.
+ */
+
+class Constants {
+    public static final String FOLDER_NAME ="/FBDownloader/";
+    public static String MyPREFERENCES = "PREFS";
+}
